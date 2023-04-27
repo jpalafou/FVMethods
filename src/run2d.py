1604b248a131477417c79eb1d697311a0fc51b7c
Jonathan Palafoutas <jpalafou@princeton.edu>
2023-04-27 05:46:31 -0400

    reformatted 2d advection

--- src/run2d.py
+++ src/run2d.py
@@ -1,61 +1,17 @@
 import warnings
-import util.advection2d as advection2d
+from util.advection2d import AdvectionSolver
 
 warnings.filterwarnings("ignore")
 
-# inputs
-ic_type = "square"  # initial condition type
-n = 64  # number of cells in both directions
-a = [1, 1]  # tranpsort speed
-x_bounds = [0, 1]  # spatial domain
-T = 1  # solving time
-
-# configurations
-spatial_order = 4
-time_order = 4
-courant_factor = 0.5
-solution_scheme = "no limiter"
-
-# array of x-values
-x_interface = np.linspace(x_bounds[0], x_bounds[1], num=n + 1)
-x = 0.5 * (x_interface[:-1] + x_interface[1:])  # x at cell centers
-h = (x_bounds[1] - x_bounds[0]) / n
-
-# time vector
-rkorder = time_order
-Dt = courant_factor * h / max(np.abs(a))
-if time_order > 4:
-    rkorder = 4
-    time_step_adjustment = rk4_Dt_adjust(
-        h, x_bounds[1] - x_bounds[0], spatial_order
-    )
-    Dt = Dt * time_step_adjustment
-n_time = int(np.ceil(T / Dt))
-t = np.linspace(0, T, num=n_time)
-
-# initial condition
-u0 = initial_condition2d(x, ic_type)
-
-# set up solution
-if solution_scheme == "no limiter":
-    solution = advection2d.AdvectionSolver(
-        u0=u0, t=t, h=h, a=a[0], b=a[1], order=spatial_order
-    )
-else:
-    raise BaseException(f"invalid solution scheme {solution_scheme}")
-# time integration
-solution.rkn(rkorder)
-
-# plot
-fig, ((ax0, ax1, ax2)) = plt.subplots(1, 3, figsize=(12, 10))
-im0 = ax0.imshow(solution.u[0])
-cbar0 = ax0.figure.colorbar(im0, ax=ax0, shrink=0.5)
-ax0.set_title("initial condition")
-im1 = ax1.imshow(solution.u[-1])
-cbar1 = ax1.figure.colorbar(im1, ax=ax1, shrink=0.5)
-ax1.set_title("final time step")
-im2 = ax2.imshow(solution.u[-1] - solution.u[0])
-cbar2 = ax2.figure.colorbar(im2, ax=ax2, shrink=0.5)
-ax2.set_title("initial condition - final time step")
-fig.tight_layout()
-plt.show()
+solution = AdvectionSolver(
+    u0_preset="square",
+    n=(32, 32),
+    x=(0, 1),
+    y=(0, 1),
+    T=2,
+    a=(1, 1),
+    courant=0.5,
+    order=4,
+)
+solution.rkorder()
+solution.plot_error()
--- src/util/advection2d.py
+++ src/util/advection2d.py
@@ -2,239 +2,241 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from util.initial_condition import initial_condition2d
-from util.integrate import Integrator, rk4_Dt_adjust
+from util.integrate import Integrator
 from util.fvscheme import ConservativeInterpolation
-from util.david.simple_trouble_detection import trouble_detection
 
 
+def rk4_Dt_adjust(h, L, order):
+    """
+    args:
+        h:  cell size
+        L:  1d domain size
+        order:  accuracy requirement
+    returns:
+        Dt multiplication factor for rk4 to satisfy an order of accuracy
+    """
+    return (h / L) ** ((order - 4) / 4)
+
+
+# class definition
 class AdvectionSolver(Integrator):
     """
-    uses polynomial reconstruction of arbitrary order
-    no limiter
+    args:
+        u0_preset   string describing a pre-coded initial condition
+        n:  tuple of number of cells in x and y
+        x:  tuple of boundaries in x
+        y:  tuple of boundaries in y
+        T:  solving time
+        a:  tuple of constant advection speed in x and y or callable function
+        a_max:  tuple of abs max advection speeds if a is callable
+        courant:    stability condition
+        order:  accuracy requirement for polynomial interpolation
+        bc_type:    string describing a pre-coded boudnary condition
+        loglen: number of saved states
+        adujst_time_step:   whether to reduce timestep for order >4
+    returns:
+        u   array of saved states
     """
 
     def __init__(
         self,
-        u0: np.ndarray = None,
         u0_preset: str = "square",
-        n: tuple = (32, 32),
+        n: tuple = 32,
         x: tuple = (0, 1),
-        y: tuple = (0, 1),
-        T: float = 2,
+        y: tuple = None,
+        T: float = 1,
         a: tuple = (1, 1),
+        a_max: float = None,
         courant: float = 0.5,
         order: int = 1,
         bc_type: str = "periodic",
-        posteriori: bool = False,
+        loglen: int = 11,
+        adujst_time_step: bool = False,
     ):
-        self.a = a[0]
-        self.b = a[1]
+        # misc. attributes
         self.order = order
+        self.adujst_time_step = adujst_time_step
 
         # spatial discretization
-        if isinstance(n, int):
-            self.nx = n
-            self.ny = n
-        elif isinstance(n, tuple):
-            self.nx = n[0]
-            self.ny = n[1]
-        self.x_interface = np.linspace(x[0], x[1], num=self.nx + 1)
+        self.n = (n, n) if isinstance(n, int) else n
+        nx, ny = self.n
+        y = x if y is None else y
+        self.x_interface = np.linspace(
+            x[0], x[1], num=nx + 1
+        )  # cell interfaces
         self.x = 0.5 * (
             self.x_interface[:-1] + self.x_interface[1:]
-        )  # x at cell centers
-        self.hx = (x[1] - x[0]) / self.nx
-        self.y_interface = np.linspace(y[0], y[1], num=self.ny + 1)
+        )  # cell centers
+        self.Lx = x[1] - x[0]  # domain size in x
+        self.hx = self.Lx / nx  # cell size in x
+        self.y_interface = np.linspace(
+            y[0], y[1], num=ny + 1
+        )  # cell interfaces
         self.y = 0.5 * (
             self.y_interface[:-1] + self.y_interface[1:]
-        )  # y at cell centers
-        self.hy = (x[1] - x[0]) / self.ny
+        )  # cell centers
+        self.Ly = y[1] - y[0]  # domain size in y
+        self.hy = self.Ly / ny  # cell size in y
+
+        # constant advection velocity defined at cell interfaces
+        if isinstance(a, tuple):
+            self.a = a[0] * np.ones((ny, nx + 1))
+            self.b = a[1] * np.ones((ny + 1, nx))
+        if callable(a):
+            self.a = a
+        a_max = (1, 1) if a_max is None else a_max
 
         # time discretization
         self.courant = courant
-        if self.a:  # nonzero horizontal advection velocity
-            Dtx = courant * self.hx / np.abs(self.a)
-        else:
-            Dtx = courant * self.hx
-        if self.b:  # nonzero vertical advection velocity
-            Dty = courant * self.hy / np.abs(self.b)
-        else:
-            Dty = courant * self.hy
-        Dt = min(Dtx, Dty)
-        time_step_adjustment = 1
-        if order > 4:
-            time_step_adjustmentx = rk4_Dt_adjust(self.hx, x[1] - x[0], order)
-            time_step_adjustmenty = rk4_Dt_adjust(self.hy, y[1] - y[0], order)
-            time_step_adjustment = min(
-                time_step_adjustmentx, time_step_adjustmenty
-            )
+        Dt = courant * min(
+            self.hx / max(a_max[0], 1e-6), self.hy / max(a_max[1], 1e-6)
+        )
+        Dt_adjustment = 1
+        if self.adujst_time_step and order > 4:
+            Dt_adjustment_x = rk4_Dt_adjust(self.hx, self.Lx, order)
+            Dt_adjustment_y = rk4_Dt_adjust(self.hy, self.Ly, order)
+            Dt_adjustment = min(Dt_adjustment_x, Dt_adjustment_y)
+            Dt = Dt * Dt_adjustment
             print(
-                "Decreasing timestep by a factor of"
-                f" {time_step_adjustment} to maintain order {order} with rk4"
+                f"Decreasing timestep by a factor of {Dt_adjustment} to maintain",
+                f" order {order} with rk4",
             )
-            Dt = Dt * time_step_adjustment
-        n_time = int(np.ceil(T / Dt))
-        self.Dt = T / n_time
-        self.time_step_adjustment = time_step_adjustment
-        self.t = np.linspace(0, T, num=n_time)
+        self.Dt_adjustment = Dt_adjustment
+        n_timesteps = int(np.ceil(T / Dt))
+        self.Dt = T / n_timesteps
+        self.t = np.linspace(0, T, num=n_timesteps + 1)
 
-        # initial condition
-        if not u0:
-            self.u0 = initial_condition2d(self.x, self.y, u0_preset)
-        else:
-            if u0.shape != (self.ny, self.nx):
-                raise BaseException(f"Invalid initial condition for size {n}")
-            self.u0 = u0
+        # initial/boundary conditions
+        u0 = initial_condition2d(self.x, self.y, u0_preset)
+        self.bc_type = bc_type
 
         # initialize integrator
-        super().__init__(self.u0, self.t)
+        super().__init__(u0=u0, t=self.t, loglen=loglen)
 
-        # stensil for reconstructed values at cell interfaces
-        right_interface_stensil_original = (
-            ConservativeInterpolation.construct_from_order(order, "right")
-        )
-        left_interface_stensil_original = (
+        # interpolating values at cell interfaces
+        left_interface_stensil = (
             ConservativeInterpolation.construct_from_order(order, "left")
+            .nparray()
+            .reshape(-1, 1, 1)
         )
-        self.right_interface_stensil = (
-            right_interface_stensil_original.nparray().reshape(-1, 1, 1)
+        right_interface_stensil = (
+            ConservativeInterpolation.construct_from_order(order, "right")
+            .nparray()
+            .reshape(-1, 1, 1)
         )
-        self.left_interface_stensil = (
-            left_interface_stensil_original.nparray().reshape(-1, 1, 1)
+        self._stensil_size = len(
+            left_interface_stensil
+        )  # assume symmetric stensils
+        self.k = int(
+            np.floor(len(left_interface_stensil) / 2)
+        )  # length of stensil arms
+        self.gw = self.k + 1
+
+        # complete list of interpolation stensils going fromleft to right
+        interior_stensils = []
+        self.list_of_stensils = (
+            [left_interface_stensil]
+            + interior_stensils
+            + [right_interface_stensil]
         )
-        assert sum(self.left_interface_stensil) == sum(
-            self.right_interface_stensil
-        )
-        self._stensil_sum = sum(self.left_interface_stensil)
-        # number of kernel cells from center referenced by a scheme
-        # symmetry is assumed
-        self._k = max(right_interface_stensil_original.coeffs.keys())
-        # number of ghost cells on either side of the extended state vector
-        self._gw = self._k + 1
-        self.bc_type = bc_type  # type of boudnary condition
-        # trouble detection
-        self.trouble = np.zeros(self.u0.shape)  # troubled cells
 
     def apply_bc(
-        self, without_ghost_cells: np.ndarray, num_ghost_cells: int
+        self, u_without_ghost_cells: np.ndarray, gw: int, direction: str
     ) -> np.ndarray:
-        # construct an extended array
-        ny, nx = without_ghost_cells.shape
-        with_ghost_cells = np.zeros(
-            (ny + 2 * num_ghost_cells, nx + 2 * num_ghost_cells)
-        )
-        gw = num_ghost_cells
-        negative_gw = -gw
-        with_ghost_cells[gw:negative_gw, gw:negative_gw] = without_ghost_cells
+        """
+        args:
+            u_without_ghost_cells   1d np array
+            gw      number of ghost cells on either side of u
+            direction   x or y
+        returns:
+            u_with_ghost_cells
+        """
         if self.bc_type == "periodic":
-            left_index = -2 * gw
-            right_index = 2 * gw
-            # left/right
-            with_ghost_cells[:, :gw] = with_ghost_cells[
-                :, left_index:negative_gw
-            ]
-            with_ghost_cells[:, negative_gw:] = with_ghost_cells[
-                :, gw:right_index
-            ]
-            # up/down
-            with_ghost_cells[:gw, :] = with_ghost_cells[
-                left_index:negative_gw, :
-            ]
-            with_ghost_cells[negative_gw:, :] = with_ghost_cells[
-                gw:right_index, :
-            ]
-            return with_ghost_cells
+            if direction == "x":
+                pad_width = ((0, 0), (gw, gw))
+            if direction == "y":
+                pad_width = ((gw, gw), (0, 0))
+            u_with_ghost_cells = np.pad(
+                u_without_ghost_cells, pad_width, mode="wrap"
+            )
+        return u_with_ghost_cells
 
-    def reimann(
+    def riemann(
         self,
-        a_at_boundary,
-        value_to_the_left_of_boundary: float,
-        value_to_the_right_of_boundary: float,
+        velocities_at_boundaries: float,
+        left_of_boundary_values: float,
+        right_of_boundary_values: float,
     ) -> float:
-        if a_at_boundary > 0:
-            return value_to_the_left_of_boundary
-        elif a_at_boundary < 0:
-            return value_to_the_right_of_boundary
-        else:
-            return 0
+        fluxes = np.zeros(velocities_at_boundaries.shape)
+        fluxes = np.where(
+            velocities_at_boundaries > 0, left_of_boundary_values, fluxes
+        )
+        fluxes = np.where(
+            velocities_at_boundaries < 0, right_of_boundary_values, fluxes
+        )
+        return fluxes * velocities_at_boundaries
 
-    def udot(self, u: np.ndarray, t_i: float) -> np.ndarray:
+    def udot(self, u: np.ndarray, t_i: float, dt: float = None) -> np.ndarray:
         # construct an extended array with ghost zones and apply bc
+        u_extended_ns = self.apply_bc(u, self.gw, direction="y")
+        u_extended_ew = self.apply_bc(u, self.gw, direction="x")
         ny, nx = u.shape
-        uwithghosts = self.apply_bc(u, self._gw)
-        # construct an array of staggered state arrays such that a 3D
-        # matrix operation with a stensil multiplies weights by their
-        # referenced cells
-        # north/south east/west
-        ns = []
-        ew = []
-        stensil_length = 2 * self._k + 1
-        for i in range(stensil_length):
+        # construct an array of staggered state arrays such that a 3D matrix operation
+        # with a stensil multiplies weights by their referenced cells north/south
+        # east/west
+        ns_stack, ew_stack = [], []
+        for i in range(self._stensil_size):
             top_ind = i + ny + 2
             right_ind = i + nx + 2
-            ns.append(uwithghosts[i:top_ind, :])
-            ew.append(uwithghosts[:, i:right_ind])
-        NS = np.asarray(ns)
-        EW = np.asarray(ew)
-        ubar_north = sum(NS * self.left_interface_stensil) / self._stensil_sum
-        ubar_south = sum(NS * self.right_interface_stensil) / self._stensil_sum
-        ubar_east = sum(EW * self.right_interface_stensil) / self._stensil_sum
-        ubar_west = sum(EW * self.left_interface_stensil) / self._stensil_sum
-        # evaluation arrays have excess ghost information and must be
-        # chopped
-        chop = self._gw - 1
-        anti_chop = -chop
-        if chop > 0:
-            ubar_north = ubar_north[:, chop:anti_chop]
-            ubar_south = ubar_south[:, chop:anti_chop]
-            ubar_east = ubar_east[chop:anti_chop, :]
-            ubar_west = ubar_west[chop:anti_chop, :]
-        # initialize empty difference arrays
-        Delta_ubar_NS = np.zeros((ny, nx))
-        Delta_ubar_EW = np.zeros((ny, nx))
-        for i in range(1, ny + 1):
-            for j in range(1, nx + 1):
-                Delta_ubar_NS[i - 1, j - 1] = self.reimann(
-                    self.b,
-                    ubar_north[i, j],
-                    ubar_south[i - 1, j],
-                ) - self.reimann(
-                    self.b,
-                    ubar_north[i + 1, j],
-                    ubar_south[i, j],
-                )
-                Delta_ubar_EW[i - 1, j - 1] = self.reimann(
-                    self.a,
-                    ubar_east[i, j],
-                    ubar_west[i, j + 1],
-                ) - self.reimann(
-                    self.a,
-                    ubar_east[i, j - 1],
-                    ubar_west[i, j],
-                )
+            ew_stack.append(u_extended_ew[:, i:right_ind])
+            ns_stack.append(u_extended_ns[i:top_ind, :])
+        ns_stack, ew_stack = np.asarray(ns_stack), np.asarray(ew_stack)
+        # evaluate 1d average at each cell face
+        ubar_north = np.sum(ns_stack * self.list_of_stensils[1], axis=0) / sum(
+            self.list_of_stensils[1]
+        )
+        ubar_south = np.sum(ns_stack * self.list_of_stensils[0], axis=0) / sum(
+            self.list_of_stensils[0]
+        )
+        ubar_east = np.sum(ew_stack * self.list_of_stensils[1], axis=0) / sum(
+            self.list_of_stensils[1]
+        )
+        ubar_west = np.sum(ew_stack * self.list_of_stensils[0], axis=0) / sum(
+            self.list_of_stensils[0]
+        )
+        # solve flux at each face
+        ew_fluxes = self.riemann(self.a, ubar_east[:, :-1], ubar_west[:, 1:])
+        ns_fluxes = self.riemann(self.b, ubar_north[:-1, :], ubar_south[1:, :])
         return (
-            -(self.a / self.hx) * Delta_ubar_EW
-            - (self.b / self.hy) * Delta_ubar_NS
+            -(ew_fluxes[:, 1:] - ew_fluxes[:, :-1]) / self.hx
+            + -(ns_fluxes[1:, :] - ns_fluxes[:-1, :]) / self.hy
         )
 
-    def posteriori_revision(self, previous_solution, candidate_solution):
+    def rkorder(self):
         """
-        u1 is now the candidate solution. check if it is (what are we
-        checking?) and apply fallback scheme if necessary
+        rk integrate to an order that matches the spatial order
         """
-        # david usesa gw of 2
-        ny, nx = candidate_solution.shape
-        previous_solution = self.apply_bc(
-            without_ghost_cells=self.u0, num_ghost_cells=2
-        )
-        previous_solution = previous_solution.reshape(1, ny, nx)
-        candidate_solution = self.apply_bc(
-            without_ghost_cells=self.u1, num_ghost_cells=2
-        )
-        candidate_solution = candidate_solution.reshape(1, ny, nx)
+        if self.order > 3:
+            self.rk4()
+        elif self.order > 2:
+            self.rk3()
+        elif self.order > 1:
+            self.rk2()
+        else:
+            self.euler()
 
-        trouble_detection(self.trouble, previous_solution, candidate_solution)
-
-        print(self.trouble)
+    def find_error(self, norm: str = "l1"):
+        """
+        measure error between first state and last state
+        """
+        approx = self.u[-1]
+        truth = self.u[0]
+        if norm == "l1":
+            return np.sum(np.abs(approx - truth)) / len(truth)
+        if norm == "l2":
+            return np.sqrt(np.sum(np.power(approx - truth, 2)) / len(truth))
+        if norm == "inf":
+            return np.max(np.abs(approx - truth))
 
     def plot_error(self):
         fig, ((ax0, ax1, ax2)) = plt.subplots(1, 3, figsize=(12, 10))
