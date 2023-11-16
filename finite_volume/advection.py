35a90591e48d920c1f5a25382946e105d0953aed
Jonathan L. Palafoutas <jp7427@stellar-intel.princeton.edu>
2023-11-16 15:03:13 -0500

    fix: format

--- finite_volume/advection.py
+++ finite_volume/advection.py
@@ -1221,13 +1221,13 @@ class AdvectionSolver(Integrator):
         stats = {}
         stats["worst"] = np.min(violations)
         stats["mean"] = np.mean(violations)
-        stats["lower bound violation frequency"] = (
-            np.sum(np.where(lower_bound_violations < 0, 1, 0)) / (self.step_count + 1)
-        )
-        stats["upper bound violation frequency"] = (
-            np.sum(np.where(upper_bound_violations < 0, 1, 0)) / (self.step_count + 1)
-        )
-        stats["violation frequency"] = (
-            np.sum(np.where(violations < 0, 1, 0)) / (self.step_count + 1)
+        stats["lower bound violation frequency"] = np.sum(
+            np.where(lower_bound_violations < 0, 1, 0)
+        ) / (self.step_count + 1)
+        stats["upper bound violation frequency"] = np.sum(
+            np.where(upper_bound_violations < 0, 1, 0)
+        ) / (self.step_count + 1)
+        stats["violation frequency"] = np.sum(np.where(violations < 0, 1, 0)) / (
+            self.step_count + 1
         )
         return violations, stats
